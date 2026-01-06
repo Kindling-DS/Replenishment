c50aefc5bbd115d8832052e70e48c89745ac99e0
Kindling-DS <analyst@shopkindling.ca>
2026-01-06 10:08:08 -0500

    Update secrets.py

--- config/secrets.py
+++ config/secrets.py
@@ -1,1 +1,15 @@
+import os
+
+COVA_CLIENT_ID = os.getenv("COVA_CLIENT_ID")
+COVA_CLIENT_SECRET = os.getenv("COVA_CLIENT_SECRET")
+COVA_USERNAME = os.getenv("COVA_USERNAME")
+COVA_PASSWORD = os.getenv("COVA_PASSWORD")
+
+OCS_EMAIL = os.getenv("OCS_EMAIL")
+OCS_PASSWORD = os.getenv("OCS_PASSWORD")
+
+MYSQL_HOST = os.getenv("MYSQL_HOST")
+MYSQL_DB = os.getenv("MYSQL_DB")
+MYSQL_USER = os.getenv("MYSQL_USER")
+MYSQL_PASSWORD = os.getenv("MYSQL_PASSWORD")
 
